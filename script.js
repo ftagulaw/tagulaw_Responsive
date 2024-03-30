851b268e9734c28fcbfbedc6748304b1bceb5f1d
ftagulaw <160147308+ftagulaw@users.noreply.github.com>
2024-03-30 12:28:18 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('menu_btn');
-const nav = document.getElementById('menu');
-
-btn.addEventListener('click', () => { 
-    btn.classList.toggle('open')
-    btn.classList.toggle('flex')
-    btn.classList.toggle('hidden')
-});
